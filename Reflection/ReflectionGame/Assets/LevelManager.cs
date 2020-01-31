d0397680e380669fd6ee2b99a85d05e428386e54
Buscems <46265895+Buscems@users.noreply.github.com>
2020-01-30 21:50:56 -0500

    Yep

---
+++ Reflection/ReflectionGame/Assets/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public MainPlayer player1, player2;
+
+    public int currentLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.ready && player2.ready)
+        {
+            switch (currentLevel)
+            {
+                case 1:
+                    SceneManager.LoadScene("Level2");
+                    break;
+                case 2:
+                    SceneManager.LoadScene("Level3");
+                    break;
+            }
+        }
+    }
+}
