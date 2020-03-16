5685863bb4930fb8fa557a4d7d3bbd63b420a3e6
Buscems <46265895+Buscems@users.noreply.github.com>
2020-03-16 06:48:50 -0400

    Not working

---
+++ Week7/NonStandardInput/Assets/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    public ParticleSystem psLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        psLeft.startColor = this.GetComponent<SpriteRenderer>().color;
+    }
+}
--- Week7/NonStandardInput/Assets/ControllerDetection.cs
+++ Week7/NonStandardInput/Assets/ControllerDetection.cs
@@ -7,6 +7,14 @@ using Rewired.ControllerExtensions;
 public class ControllerDetection : MonoBehaviour
 {
 
+    public GameObject firstCircle, secondCircle;
+
+    public GameObject xbox, playstation;
+
+    public GameObject psLeft, psRight;
+
+    bool xboxIn, playstationIn;
+
     void Awake()
     {
         // Subscribe to events
@@ -17,23 +25,82 @@ public class ControllerDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        xbox.SetActive(false);
+        playstation.SetActive(false);
+
+        psLeft.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (xboxIn)
+        {
+            xbox.SetActive(true);
+            psLeft.SetActive(true);
+        }
+        else
+        {
+            xbox.SetActive(false);
+            psLeft.SetActive(false);
+        }
+
+        /*
+        string[] names = Input.GetJoystickNames();
+        for (int x = 0; x < names.Length; x++)
+        {
+            print(names[x].Length);
+            if (names[x].Length == 19)
+            {
+                print("PS4 CONTROLLER IS CONNECTED");
+                //PS4_Controller = 1;
+                //Xbox_One_Controller = 0;
+                playstationIn = true;
+            }
+            else
+            {
+                playstationIn = false;
+            }
+            if (names[x].Length == 33)
+            {
+                print("XBOX ONE CONTROLLER IS CONNECTED");
+                //set a controller bool to true
+                //PS4_Controller = 0;
+                //Xbox_One_Controller = 1;
+                xboxIn = true;
+            }
+            else
+            {
+                xboxIn = false;
+            }
+        }
+        */
     }
 
     void OnControllerConnected(ControllerStatusChangedEventArgs args)
     {
         Debug.Log("A controller was connected! Name = " + args.name + " Id = " + args.controllerId + " Type = " + args.controllerType);
+
+        if(args.name == "XInput Gamepad 1")
+        {
+            xboxIn = true;
+        }
+        
+        
     }
 
     void OnControllerDisconnected(ControllerStatusChangedEventArgs args)
     {
         Debug.Log("A controller was disconnected! Name = " + args.name + " Id = " + args.controllerId + " Type = " + args.controllerType);
+
+        
+        if (args.name == "XInput Gamepad 1")
+        {
+            xboxIn = false;
+        }
+        
     }
 
 }
