1bdbbc6ac2865b8f3d14fea111e7b3ebacd27740
Buscems <46265895+Buscems@users.noreply.github.com>
2020-03-15 21:28:57 -0400

    Here we go part II

---
+++ Week7/NonStandardInput/Assets/Scripts/ForegroundDissolve.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundDissolve : MonoBehaviour
+{
+
+    Material dissolve;
+
+    [Header("Dissolve Variables")]
+    public float dissolveSpeed;
+    Vector2 dissolveMinMax;
+
+    float fadeValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dissolve = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        dissolve.SetFloat("_Fade", fadeValue);
+
+    }
+}
